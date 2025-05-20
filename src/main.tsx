3d5ec3cf758ceecc9471851fe775d762145f901b
it1shka <2tishbel@gmail.com>
2025-05-20 16:31:20 +0200

    feat(deps): added TanStack Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRouter, RouterProvider } from '@tanstack/react-router'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { routeTree } from './routeTree.gen'
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')
+if (rootElement === null) {
+  throw new Error('Failed to find #root element')
+}
+if (rootElement.innerHTML === '') {
+  const root = createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
---
+++ src/pages/menu/index.tsx
@@ -0,0 +1,5 @@
+const MenuPage = () => {
+  return <div>Menu Page: TODO</div>
+}
+
+export default MenuPage
